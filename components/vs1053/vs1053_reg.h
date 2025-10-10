9d3171f2074fad39ca9da8d32b7436dfeb7edff2
Tucker Kern <mill1000@users.noreply.github.com>
2025-10-10 21:13:57 +0000

    Add extra parameters struct

--- components/vs1053/vs1053_reg.h
+++ components/vs1053/vs1053_reg.h
@@ -43,5 +43,42 @@ static constexpr const uint8_t MODE_SM_ADPCM = 0x1000;     // PCM/ADPCM recordin
 static constexpr const uint8_t MODE_SM_LINE1 = 0x4000;     // MIC/LINE1 selector, 0: MICP, 1: LINE1
 static constexpr const uint8_t MODE_SM_CLKRANGE = 0x8000;  // Input clock range, 0: 12..13 MHz, 1: 24..26 MHz
 
+static constexpr const uint8_t EXTRA_PARAMETER_VERSION = 0x0003;
+static constexpr const uint8_t EXTRA_PARAMETER_ADDR = 0x1E02;
+
+typedef struct extra_parameters_t {
+  uint16_t version;         // 0x1E02 - Structure version
+  uint16_t config1;         // 0x1E03 - PPSs RRRR: PS mode, SBR mode, Reverb
+  uint16_t play_speed;      // 0x1E04 - 0,1 = Normal speed; 2 = Twice; 3 = Three times, etc.
+  uint16_t byte_rate;       // 0x1E05 - Average byte rate
+  uint16_t end_fill_byte;   // 0x1E06 - Byte value to send after file sent
+  uint16_t reserved[16];    // 0x1E07–0x1E15 - File byte offsets
+  uint32_t jump_points[8];  // 0x1E16–0x1E25 - File byte offsets
+  uint16_t latest_jump;     // 0x1E26 - Index of the last updated jump point
+  uint32_t position_ms;     // 0x1E27–0x1E28 - Play position, if known (WMA, Ogg Vorbis)
+  int16_t resync;           // 0x1E29 - >0 for automatic M4A, ADIF, WMA resyncs
+  union {
+    struct {
+      uint32_t cur_packet_size;  // 0x1E2A - Current packet size
+      uint32_t packet_size;      // 0x1E2B - Packet size
+    } wma;
+    struct {
+      uint16_t sce_found_mask;     // 0x1E2A - SCEs found since last clear
+      uint16_t cpe_found_mask;     // 0x1E2B - CPEs found since last clear
+      uint16_t lfe_found_mask;     // 0x1E2C - LFEs found since last clear
+      uint16_t play_select;        // 0x1E2D - 0 = First any, initialized at AAC init
+      int16_t dyn_compress;        // 0x1E2E - -8192 = 1.0, initialized at AAC init
+      int16_t dyn_boost;           // 0x1E2F - 8192 = 1.0, initialized at AAC init
+      uint16_t sbr_and_ps_status;  // 0x1E30 - 1 = SBR, 2 = Upsample, 4 = PS, 8 = PS active
+    } aac;
+    struct {
+      uint32_t bytes_left;  // 0x1E2A - Bytes left
+    } midi;
+    struct {
+      int16_t gain;  // 0x1E2A - Proposed gain offset in 0.5 dB steps, default = -12
+    } vorbis;
+  } codec;
+} extra_parameters_t;
+
 }  // namespace vs1053
 }  // namespace esphome
